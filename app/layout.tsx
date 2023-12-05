2394df0997f96c32b1d1925234c3615770910562
Shereni <info@aewebsites.co.zw>
2023-12-05 23:04:58 +0200

    added seo stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-
+// Seo stuff (research og link meta properties)(to check metadata for twitter etc go to opengraph.xyz)
 export const metadata: Metadata = {
   title: 'NextJs App',
   description: 'PDF Coding Resources For Beginners',
-}
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark-only',
+    'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'twitter:card': 'summary_large_image',
+    'og:url': 'jsmastery.pro',
+    'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'og:type': 'website',
+  },
+};
 
 export default function RootLayout({
   children,
